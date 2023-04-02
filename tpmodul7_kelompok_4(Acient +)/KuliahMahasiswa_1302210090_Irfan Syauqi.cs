6fbe7f42f29b2dea8873ce1e54af137b5f4790eb
Rzv888 <udinamin445@gmail.com>
2023-04-02 22:39:25 +0700

    menambahkan class KuliahMahasiswa_1302210090_Irfan_Syauqi

---
+++ tpmodul7_kelompok_4(Acient +)/KuliahMahasiswa_1302210090_Irfan Syauqi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_4_Acient___
+{
+    internal class KuliahMahasiswa_1302210090_Irfan_Syauqi
+    {
+        public class MataKuliah
+        {
+            public string KodeMK { get; set; }
+            public string NamaMK { get; set; }
+        }
+
+        public class KuliahMahasiswa_1302210090
+        {
+            public List<MataKuliah> DaftarMK { get; set; }
+
+            public void ReadJSON()
+            {
+                try
+                {
+                    string jsonString = File.ReadAllText("tp7_2_1302210090.json");
+                    KuliahMahasiswa_1302210090 data = JsonSerializer.Deserialize<KuliahMahasiswa_1302210090>(jsonString);
+                    Console.WriteLine("Daftar mata kuliah yang diambil:");
+                    int i = 1;
+                    foreach (MataKuliah mk in data.DaftarMK)
+                    {
+                        Console.WriteLine("MK {0} {1} - {2}", i, mk.KodeMK, mk.NamaMK);
+                        i++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: {0}", ex.Message);
+                }
+            }
+        }
+    }
+}
+
--- tpmodul7_kelompok_4(Acient +)/Program.cs
+++ tpmodul7_kelompok_4(Acient +)/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static tpmodul7_kelompok_4_Acient___.KuliahMahasiswa_1302210090_Irfan_Syauqi;
+
+namespace tpmodul7_kelompok_4_Acient___
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        KuliahMahasiswa_1302210090 data = new KuliahMahasiswa_1302210090 ();
+        data.ReadJSON();
+        Console.ReadKey();
+    }
+}
+}
