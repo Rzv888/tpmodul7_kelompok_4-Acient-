7ac875778e0e0033297e2e30f00eeaa6b62345ee
atiohaidar <atiohaidar@gmail.com>
2023-04-02 23:59:46 +0700

    menambahkan class <DataMahasiswa_1302213109_Muhammad_Burhan>

---
+++ tpmodul7_kelompok_4(Acient +)/DataMahasiswa_1302213109_Muhammad_Burhan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace tpmodul7_kelompok_4_Acient___
+{
+    internal class DataMahasiswa_1302213109_Muhammad_Burhan
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+        public string Fakultas { get; set; }
+
+        public void ReadJSON()
+        {
+            try
+            {
+                string jsonString = File.ReadAllText("tp7_1_1302213109.json");
+                DataMahasiswa_1302213109_Muhammad_Burhan data = JsonSerializer.Deserialize<DataMahasiswa_1302213109_Muhammad_Burhan>(jsonString);
+                Console.WriteLine("Nama {0} dengan nim {1} dari fakultas {2}", data.Nama, data.NIM, data.Fakultas);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+        } 
+
+    }
+}
--- tpmodul7_kelompok_4(Acient +)/Program.cs
+++ tpmodul7_kelompok_4(Acient +)/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul7_kelompok_4_Acient___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa_1302213109_Muhammad_Burhan data = new DataMahasiswa_1302213109_Muhammad_Burhan();
+            data.ReadJSON();
+            Console.ReadKey();
+        }
+    }
+}
